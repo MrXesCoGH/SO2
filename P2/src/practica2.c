a0ee6a6a8b95a8522cefeee48fa5c8e57faeb3de
Fran Díaz <MrXesCoGH@users.noreply.github.com>
2018-10-16 11:10:10 +0200

    DONE: Avance en lectura de fichero dades.csv

--- P2/src/practica2.c
+++ P2/src/practica2.c
@@ -5,7 +5,14 @@
 #include "/arbre-binari/red-black-tree.h"
 #include "/linked-list/linked-list.h"
 
-void lectura(String aeroports_path, String dades_path, rb_tree *a_tree, list *d_list){
+void process(String aeroports_path, String dades_path, String origin){
+  tree *aeroports_tree;
+  node_data *n_data;
+  /* Allocate memory for tree */
+  aeroports_tree = (rb_tree *) malloc(sizeof(rb_tree));
+  /* Initialize the tree */
+  init_tree(aeroports_tree);
+
   // Reading file process.
   FILE *fp;
 	char str[100];
@@ -28,8 +35,27 @@ void lectura(String aeroports_path, String dades_path, rb_tree *a_tree, list *d_
         int h = strlen(str);
 		    //turn the \n to 0
     		str[h-1] = '\0';
-        //This creates a new node in the tree where it'll store the string.
-    	  insert_node(a_tree, str);
+        //This search if the str value is already inside the tree.
+        n_data = find_node(aeroports_tree, str);
+
+        if (n_data != NULL) {
+          /* If the key is in the tree increment 'num' */
+          n_data->num_vegades++;
+        } else {
+          
+          list d_list;
+          d_list = (list*) malloc(sizeof(list));
+          init_list(d_list);
+
+          /* If the key is not in the tree, allocate memory for the data
+           * and insert in the tree */
+          n_data = malloc(sizeof(node_data));
+          /* This is the key by which the node is indexed in the tree */
+          n_data->key = str;
+          /* This is additional information that is stored in the tree */
+          n_data->num_vegades = 1;
+          n_data->list = d_list;
+  	      insert_node(aeroports_tree, n_data);
 	}
 
 	fclose(fp);
@@ -46,6 +72,16 @@ void lectura(String aeroports_path, String dades_path, rb_tree *a_tree, list *d_
 
 	int MIDA_DADES = atoi(str); //This int stores the total number of lines that We read.
 	int i = 0; //This is just a simple counter used to iterate in the following loop.
+  char **dades_list = malloc(MIDA_DADES*sizeof(char*));
+
+  /*
+  * Now that the dades file is opened, I need to read the columns.
+  * In this case, the columns are:  15, 17, 18.
+  * Each column has an especified data:
+  *   - col. 15: Delay to the arriving airport, in minutes (int)
+  *   - col. 17: Origin airport, IATA Code.
+  *   - col. 18: Destiny airport, IATA Code.
+  */
 
 	while(fgets(str, MIDA_DADES, fp) != NULL && i < MIDA_DADES){
         //Here it's stored the length of the char array.
@@ -71,23 +107,13 @@ int main(int argc, char **argv[]){
   * introduced are less than the needed will trigger the error of lacking param.
   */
 
-  rb_tree *aeroports_tree; //Tree which will be used to make every search.
-  list *dades_list; //linked list with the data from every airport.
-  char *origen; //char array where it'll be stored the origin argument.
-
   if(argc == 4){
-    /* Allocate memory for tree */
-    aeroports_tree = (rb_tree *) malloc(sizeof(rb_tree));
-    /* Initialize the tree */
-    init_tree(aeroports_tree);
-
-    /*Initialize the list*/
-    dades_list = (list *) malloc(sizeof(dades_list));
-    init_list(dades_list);
-
     //This reads every file needed in order to get the data we're going to use.
-    lectura(argv[1], argv[2], &aeroports_tree, &dades_list);
-    origen = argv[3]; //This is the origin aeroport especified by the user.
+    //argv[1]: aeroports.csv path
+    //argv[2]: dades.csv path
+    //argv[3]: origen
+    process(argv[1], argv[2], argv[3]);
+
   }
 
   // In case there are less parameters than needed.
@@ -99,17 +125,6 @@ int main(int argc, char **argv[]){
   else{
     printf("ERR: Hi han parametres de mes");
   }
-
-  /*
-  * Now that the files were read, now we need to read the columns of the
-  * dades.csv in order to obtain the especified data we need to use for
-  * the program. In this case, the columns are:  15, 17, 18.
-  * Each column has an especified data:
-  *   - col. 15: Delay to the arriving airport, in minutes (int)
-  *   - col. 17: Origin airport, IATA Code.
-  *   - col. 18: Destiny airport, IATA Code.
-  */
-
 
   /* Delete the tree */
   delete_tree(tree);
